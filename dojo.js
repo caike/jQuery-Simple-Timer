69d7ef1c18a1c59728be06e0882db180d55194c3
Carlos Souza <carloshrsouza@gmail.com>
2014-04-18 19:13:49 -0400

    Make text bigger and click for reload.

--- dojo.js
+++ dojo.js
@@ -6,15 +6,6 @@ $(function(){
       // this allows for things like element.addClass('...');
 
       $('html, body').addClass('bodyTimeoutBackground');
-
-      var restartButton = document.createElement('input')
-
-      restartButton.type = 'button';
-      restartButton.value = 'Restart';
-      restartButton.onclick = function(){
-        return location.reload();
-      }
-      $('.actions').append(restartButton);
     }
-  });
+  }).click(function(){ location.reload() });
 })
