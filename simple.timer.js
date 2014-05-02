66412b84f4480ffb8146722d992ff48ff6465c6d
Carlos Souza <carloshrsouza@gmail.com>
2014-05-01 23:10:15 -0400

    Extract logic to Timer object.

--- simple.timer.js
+++ simple.timer.js
@@ -8,103 +8,18 @@
 *
 */
 (function($){
-  $.fn.startTimer = function(options) {
 
-    var that = this;
+  var Timer = function(targetElement){
+    this.targetElement = targetElement;
+    return this;
+  };
 
-    var currentTime = function() {
-      return Math.round((new Date()).getTime() / 1000);
-    };
+  Timer.start = function(options, targetElement){
+    var timer = new Timer(targetElement);
+    return timer.start(options);
+  };
 
-    var lpad = function(n, width) {
-      width = width || 2;
-      n = n + '';
-
-      var padded = null;
-
-      if (n.length >= width) {
-        padded = n;
-      } else {
-        padded = Array(width - n.length + 1).join(0) + n;
-      }
-
-      return padded;
-    };
-
-    var formatTimeLeft = function(timeLeft) {
-      var hours, minutes, remaining, seconds;
-      remaining = new Date(timeLeft * 1000);
-      hours = remaining.getUTCHours();
-      minutes = remaining.getUTCMinutes();
-      seconds = remaining.getUTCSeconds();
-
-      if (+hours === 0 && +minutes === 0 && +seconds === 0) {
-        return [];
-      } else {
-        return [lpad(hours), lpad(minutes), lpad(seconds)];
-      }
-    };
-
-    var setFinalValue = function(finalValues, element) {
-
-      if(finalValues.length === 0){
-        clearTimer(element);
-        element.trigger('complete');
-        return false;
-      }
-
-      element.find('.seconds').text(finalValues.pop());
-      element.find('.minutes').text(finalValues.pop() + ':');
-      element.find('.hours').text(finalValues.pop() + ':');
-    };
-
-    var clearTimer = function(element){
-      element.find('.seconds').text('00');
-      element.find('.minutes').text('00:');
-      element.find('.hours').text('00:');
-    };
-
-    var fetchSecondsLeft = function(element){
-      var secondsLeft = element.data('seconds-left');
-      var minutesLeft = element.data('minutes-left');
-
-      if(secondsLeft){
-        return parseInt(secondsLeft, 10);
-      } else if(minutesLeft) {
-        return parseFloat(minutesLeft) * 60;
-      }else {
-        throw 'Missing time data';
-      }
-    };
-
-
-    var startCountdown = function(element, options) {
-      options = options || {};
-
-      var intervalId = null;
-      var defaultComplete = function(){
-        clearInterval(intervalId);
-        return clearTimer(element);
-      };
-
-      element.onComplete = options.onComplete || defaultComplete;
-
-      var timeLeft = null;
-      var secondsLeft = fetchSecondsLeft(element);
-
-      var refreshRate = options.refreshRate || 1000;
-      var endTime = secondsLeft + currentTime();
-      var timeLeft = endTime - currentTime();
-
-      setFinalValue(formatTimeLeft(timeLeft), element);
-
-      intervalId = setInterval((function() {
-        timeLeft = endTime - currentTime();
-        setFinalValue(formatTimeLeft(timeLeft), element);
-      }), refreshRate);
-
-      element.intervalId = intervalId;
-    };
+  Timer.prototype.start = function(options) {
 
     var createSubDivs = function(timerBoxElement){
       var seconds = document.createElement('div');
@@ -126,7 +41,7 @@
         append(clearDiv);
     };
 
-    this.each(function(_index, timerBox) {
+    this.targetElement.each(function(_index, timerBox) {
       var timerBoxElement = $(timerBox);
 
       timerBoxElement.on('complete', function() {
@@ -142,9 +57,106 @@
       });
 
       createSubDivs(timerBoxElement);
-      return startCountdown(timerBoxElement, options);
-    });
+      return this.startCountdown(timerBoxElement, options);
+    }.bind(this));
+  };
 
+  Timer.prototype.fetchSecondsLeft = function(element){
+    var secondsLeft = element.data('seconds-left');
+    var minutesLeft = element.data('minutes-left');
+
+    if(secondsLeft){
+      return parseInt(secondsLeft, 10);
+    } else if(minutesLeft) {
+      return parseFloat(minutesLeft) * 60;
+    }else {
+      throw 'Missing time data';
+    }
+  };
+
+  Timer.prototype.startCountdown = function(element, options) {
+    options = options || {};
+
+    var intervalId = null;
+    var defaultComplete = function(){
+      clearInterval(intervalId);
+      return this.clearTimer(element);
+    }.bind(this);
+
+    element.onComplete = options.onComplete || defaultComplete;
+
+    var secondsLeft = this.fetchSecondsLeft(element);
+
+    var refreshRate = options.refreshRate || 1000;
+    var endTime = secondsLeft + this.currentTime();
+    var timeLeft = endTime - this.currentTime();
+
+    this.setFinalValue(this.formatTimeLeft(timeLeft), element);
+
+    intervalId = setInterval((function() {
+      timeLeft = endTime - this.currentTime();
+      this.setFinalValue(this.formatTimeLeft(timeLeft), element);
+    }.bind(this)), refreshRate);
+
+    element.intervalId = intervalId;
+  };
+
+  Timer.prototype.clearTimer = function(element){
+    element.find('.seconds').text('00');
+    element.find('.minutes').text('00:');
+    element.find('.hours').text('00:');
+  };
+
+  Timer.prototype.currentTime = function() {
+    return Math.round((new Date()).getTime() / 1000);
+  };
+
+  Timer.prototype.formatTimeLeft = function(timeLeft) {
+
+    var lpad = function(n, width) {
+      width = width || 2;
+      n = n + '';
+
+      var padded = null;
+
+      if (n.length >= width) {
+        padded = n;
+      } else {
+        padded = Array(width - n.length + 1).join(0) + n;
+      }
+
+      return padded;
+    };
+
+    var hours, minutes, remaining, seconds;
+    remaining = new Date(timeLeft * 1000);
+    hours = remaining.getUTCHours();
+    minutes = remaining.getUTCMinutes();
+    seconds = remaining.getUTCSeconds();
+
+    if (+hours === 0 && +minutes === 0 && +seconds === 0) {
+      return [];
+    } else {
+      return [lpad(hours), lpad(minutes), lpad(seconds)];
+    }
+  };
+
+  Timer.prototype.setFinalValue = function(finalValues, element) {
+
+    if(finalValues.length === 0){
+      this.clearTimer(element);
+      element.trigger('complete');
+      return false;
+    }
+
+    element.find('.seconds').text(finalValues.pop());
+    element.find('.minutes').text(finalValues.pop() + ':');
+    element.find('.hours').text(finalValues.pop() + ':');
+  };
+
+
+  $.fn.startTimer = function(options) {
+    Timer.start(options, this);
     return this;
   };
 })(jQuery);
